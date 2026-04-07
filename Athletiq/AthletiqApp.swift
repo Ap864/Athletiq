5340e8df1e7965da766bb5398852a9ffb40ebf8e
Ap864 <aneeshvlogs@gmail.com>
2026-04-07 09:20:01 -0500

    Initial Commit

---
+++ Athletiq/AthletiqApp.swift
@@ -0,0 +1,17 @@
+//
+//  AthletiqApp.swift
+//  Athletiq
+//
+//  Created by 64005540 on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct AthletiqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
